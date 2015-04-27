2caa34eff9ce21f9da9f58ee214d48688cfaff4a
Snakebee <fjf.colourless@gmail.com>
2015-04-27 15:56:50 -0700

    Abstracted Database Connection

---
+++ src/DBConn.java
@@ -0,0 +1,74 @@
+import java.sql.*;
+
+public class DBConn 
+{
+	static final String DB_URL = "jdbc:mysql://sql3.freemysqlhosting.net:3306/sql367377";//hostname
+	static final String USER = "sql367377";//username
+	static final String PASS = "jT4!iX8!";//password
+	
+	private Connection conn;
+	
+	public DBConn() 
+	{
+		conn = null;
+		
+		try {
+			conn = getConnection();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
+	}
+	
+	public void query(String sql)
+	{	
+		Statement stmt;
+		try {
+			stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			printResult(rs);
+			
+			if(stmt!= null) 
+				stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void closeConnection()
+	{
+		if(conn!= null)
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+	}
+	
+	private static void printResult(ResultSet rs)
+	{
+		try {
+			while(rs.next())
+			{
+				String name = rs.getString("username");
+				System.out.println(name);
+			}
+			
+			if(rs!= null) 
+				rs.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static Connection getConnection() throws SQLException
+	{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		return DriverManager.getConnection(DB_URL, USER, PASS);
+	}
+}
--- src/TV_WatchList.java
+++ src/TV_WatchList.java
@@ -1,4 +1,3 @@
-import java.sql.*;
 public class TV_WatchList 
 {
 	/*
@@ -10,62 +9,10 @@ public class TV_WatchList
 	 * 3.) Delete row
 	 */
 	
-	static final String DB_URL = "jdbc:mysql://sql3.freemysqlhosting.net:3306/sql367377";//hostname
-	static final String USER = "sql367377";//username
-	static final String PASS = "jT4!iX8!";//password
-	
 	public static void main(String[] args) 
 	{
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
+		DBConn conn = new DBConn();
 		
-		try
-		{
-			Class.forName("com.mysql.jdbc.Driver");
-			conn = DriverManager.getConnection(DB_URL, USER, PASS);
-			stmt = conn.createStatement();
-			String sql;
-			sql = "SELECT username FROM User";
-			rs = stmt.executeQuery(sql);
-
-			while(rs.next())
-			{
-				String name = rs.getString("username");
-				System.out.println(name);
-			}
-			rs.close();
-			conn.close();
-			stmt.close();
-		}
-		catch(SQLException se)
-		{
-			se.printStackTrace();
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-		finally
-		{
-			try
-			{
-				if(rs!= null) 
-					rs.close();
-			}
-			catch(SQLException se){}
-			try
-			{
-				if(stmt!= null) 
-					stmt.close();
-			}
-			catch(SQLException se){}
-			try
-			{
-				if(conn!= null) 
-					conn.close();
-			}
-			catch(SQLException se){}
-		}
+		conn.query("SELECT username FROM User");
 	}
 }
